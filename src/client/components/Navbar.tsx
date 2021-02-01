0296fc7ed92344dd52cca0d2d603645b9e8b7d1d
Johudo <dimazuev11@gmail.com>
2021-02-01 15:54:27 +1000

    Changed Navbar

--- src/client/components/Navbar.tsx
+++ src/client/components/Navbar.tsx
@@ -8,6 +8,7 @@ import SearchSVG from "../svg/search.svg";
 import CreatePlaylistSVG from "../svg/createPlaylist.svg";
 
 import "../styles/Navbar.scss";
+import { Link } from "react-router-dom";
 
 function Navbar() {
     const navbarList = [
@@ -33,16 +34,39 @@ function Navbar() {
         },
     ];
 
+    const navbarBigPlaylists = [
+        {
+            link: "/create-playlist",
+            SVG: CreatePlaylistSVG,
+            text: "Создать плейлист",
+        },
+    ];
+
+    const navbarSmallPlaylists = [
+        {
+            link: "/",
+            text: "Любимые треки",
+        },
+        {
+            link: "/",
+            text: "Плейлист",
+        },
+        {
+            link: "/",
+            text: "Плейлист",
+        },
+    ];
+
     return (
         <nav className="navbar">
-            <ul className="navbar__list">
+            <ul className="navbar__list navbar__main-list">
                 {navbarList.map((item, index) => {
                     return (
                         <li className={item.className || ""} key={index}>
-                            <a href={item.link}>
+                            <Link to={item.link}>
                                 <item.SVG />
                                 <span>{item.text}</span>
-                            </a>
+                            </Link>
                         </li>
                     );
                 })}
@@ -51,22 +75,27 @@ function Navbar() {
             <div className="navbar__playlists">
                 <h1>Плейлисты</h1>
 
+                <ul className="navbar__list navbar__playlists-list__big">
+                    {navbarBigPlaylists.map((item, index) => {
+                        return (
+                            <li key={"navbar-big-playlist-" + index}>
+                                <Link to={item.link}>
+                                    <item.SVG />
+                                    <span>{item.text}</span>
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+
                 <ul className="navbar__list">
-                    <li>
-                        <button>
-                            <CreatePlaylistSVG />
-                            <span>Создать плейлист</span>
-                        </button>
-                    </li>
-                    <li>
-                        <a href="/">Любимые треки</a>
-                    </li>
-                    <li>
-                        <a href="/">Плейлист</a>
-                    </li>
-                    <li>
-                        <a href="/">Плейлист</a>
-                    </li>
+                    {navbarSmallPlaylists.map((item, index) => {
+                        return (
+                            <li key={"navbar-small-playlist-" + index}>
+                                <Link to={item.link}>{item.text}</Link>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
         </nav>
